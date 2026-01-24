9f602b9152a494e6814f360e4d488663cd449ab1
Anish Chaulagain <anishchaulagain2058@gmail.com>
2026-01-24 21:06:33 +0545

    browser commands

---
+++ 05-browser-commands/BrowserCommands.py
@@ -0,0 +1,22 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://opensource-demo.orangehrmlive.com/")
+# driver.maximize_window()
+# driver.minimize_window()
+driver.fullscreen_window()
+time.sleep(5)
+driver.find_element(By.CSS_SELECTOR, value=".oxd-text.oxd-text--p.orangehrm-login-forgot-header").click()
+time.sleep(5)
+driver.back()
+time.sleep(5)
+driver.forward()
+time.sleep(5)
+driver.refresh()
+time.sleep(5)
+driver.close()
+input("enter")
