20a85799921a13175d2f95b115dcb380cd380cb2
Anish Chaulagain <anishchaulagain2058@gmail.com>
2026-01-27 21:14:36 +0545

    handle multiple tabs

--- 11-handle-multiple-tabs/HandleMultipleTabs.py
+++ 11-handle-multiple-tabs/HandleMultipleTabs.py
@@ -1,6 +1,7 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.maximize_window()
@@ -8,4 +9,6 @@ driver.get("https://www.selenium.dev")
 time.sleep(3)
 driver.switch_to.new_window()
 driver.get("https://www.anishchaulagain.com.np")
+# driver.find_element(By.LINK_TEXT, value='Download CV').click()
+driver.find_element(By.XPATH, value='/html/body/div[2]/div/section[1]/div/div[2]/div/div[2]/a[1]').click()
 input("enter")
